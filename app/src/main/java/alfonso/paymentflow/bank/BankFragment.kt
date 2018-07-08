e29b812d2ac46338d16fecdb687ed137be86d5e8
Alfonso Cuitiño <alfonso.cuiti@gmail.com>
2018-07-08 11:57:41 -0300

    Create screen for bank selection

---
+++ app/src/main/java/alfonso/paymentflow/bank/BankFragment.kt
@@ -0,0 +1,50 @@
+package alfonso.paymentflow.bank
+
+import alfonso.paymentflow.R
+import alfonso.paymentflow.main.NavigationProvider
+import alfonso.paymentflow.method.OptionsAdapter
+import alfonso.paymentflow.model.CardIssuer
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_option_list.*
+
+class BankFragment : Fragment() {
+    private lateinit var presenter: BankPresenter
+    private lateinit var adapter: OptionsAdapter<CardIssuer>
+    private lateinit var paymentMethodId: String
+
+    companion object {
+        const val idKey = "payment_method_id"
+        fun init(id: String): BankFragment{
+            val args = Bundle()
+            args.putString(idKey, id)
+            val fragment = BankFragment()
+            fragment.arguments = args
+            return fragment
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        presenter = BankPresenter(activity as NavigationProvider)
+        adapter = OptionsAdapter(presenter)
+        paymentMethodId = arguments?.getString(idKey)?: ""
+        return inflater.inflate(R.layout.fragment_option_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        optionsFragmentTitle.text = getString(R.string.choose_your_bank)
+        optionsList.layoutManager = LinearLayoutManager(context)
+        optionsList.adapter = adapter
+        presenter.getCardIssuers(paymentMethodId)
+                .doAfterTerminate{ optionsFragmentProgress.visibility = View.GONE }
+                .subscribe(
+                        { issuers -> adapter.list = issuers},
+                        { error -> error.printStackTrace() }
+                )
+    }
+}
---
+++ app/src/main/java/alfonso/paymentflow/bank/BankPresenter.kt
@@ -0,0 +1,22 @@
+package alfonso.paymentflow.bank
+
+import alfonso.paymentflow.BuildConfig
+import alfonso.paymentflow.OnOptionClickListener
+import alfonso.paymentflow.main.NavigationProvider
+import alfonso.paymentflow.model.CardIssuer
+import alfonso.paymentflow.network.ApiManager
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+
+class BankPresenter(private val navigationProvider: NavigationProvider): OnOptionClickListener {
+
+    override fun onOptionClick(item: Any) {
+        navigationProvider.onCardIssuerSelected(item as CardIssuer)
+    }
+
+    fun getCardIssuers(id: String): Single<List<CardIssuer>>{
+        return ApiManager.getMercadoPagoService()
+                .getCardIssuers(BuildConfig.ApiKey, id)
+                .observeOn(AndroidSchedulers.mainThread())
+    }
+}

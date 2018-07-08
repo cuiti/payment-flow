8f73702120119cdb11a8a2223baa15ad8d62f3a7
Alfonso Cuitiño <alfonso.cuiti@gmail.com>
2018-07-08 11:51:25 -0300

    Add the remaining endpoints to the service

--- app/src/main/java/alfonso/paymentflow/network/MercadoPagoApi.kt
+++ app/src/main/java/alfonso/paymentflow/network/MercadoPagoApi.kt
@@ -1,5 +1,7 @@
 package alfonso.paymentflow.network
 
+import alfonso.paymentflow.model.CardIssuer
+import alfonso.paymentflow.model.Installment
 import alfonso.paymentflow.model.PaymentMethod
 import io.reactivex.Single
 import retrofit2.http.GET
@@ -10,4 +12,13 @@ interface MercadoPagoApi {
     @GET("payment_methods")
     fun getPaymentMethods(@Query("public_key") key: String) : Single<List<PaymentMethod>>
 
+    @GET("payment_methods/card_issuers")
+    fun getCardIssuers(@Query("public_key") key: String,
+                       @Query("payment_method_id") id: String) : Single<List<CardIssuer>>
+
+    @GET("payment_methods/installments")
+    fun getInstallments(@Query("public_key") key: String,
+                        @Query("amount") amount: Float,
+                        @Query("issuer.id") issuerId: String,
+                       @Query("payment_method_id") id: String) : Single<List<Installment>>
 }
